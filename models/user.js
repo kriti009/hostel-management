5169a7ea191132e0d4381f63a2e9e25d6de3b350
kriti009 <kriti.ritu.dew@gmail.com>
2019-02-25 13:07:17 +0530

    authentication

--- models/user.js
+++ models/user.js
@@ -3,7 +3,14 @@ var passportLocalMongoose = require("passport-local-mongoose");
 
 var userSchema = new mongoose.Schema({
    username: String,
-   password: String
+   password: String,
+   name : String,
+   email: String,
+   roll: String,
+   mobile: String,
+   branch: String,
+   sem : String,
+   gender: String
 });
 userSchema.plugin(passportLocalMongoose);
 

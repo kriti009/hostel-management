c9b35b0bf96c8d0097feed9f60034f0443814e58
kriti009 <kriti.ritu.dew@gmail.com>
2019-02-25 10:22:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+app.get("*", function(req, res){
+    res.send("SOORY!! Page Not Found");
+});
+app.listen(process.env.PORT || 8000,function(){
+    console.log("Server Connected!!");
+});
